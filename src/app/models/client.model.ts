0ecadd4124ffe7eb309c6f583fe4dad757c00e74
Adriano Dramisino <dramisinoadriano@gmail.com>
2021-02-09 22:50:33 -0500

    Started adding models.

---
+++ src/app/models/client.model.ts
@@ -0,0 +1,10 @@
+export class Client {
+    // TODO: implement like this https://nehalist.io/working-with-models-in-angular/
+    id: string;
+    name: string;
+    email: string;
+
+    constructor() {
+
+    }
+}
